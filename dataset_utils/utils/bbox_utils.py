e7abac65bc3caabfe4e4e88eada76a2b6c68dd21
Khiem Le <theinnocentman1@gmail.com>
2024-04-15 15:35:13 +0700

    feat(dataset_utils): add yolo2xyxy

--- dataset_utils/utils/bbox_utils.py
+++ dataset_utils/utils/bbox_utils.py
@@ -11,3 +11,18 @@ def xywh2yolo(
     w = box_w / img_w
     h = box_h / img_h
     return x_center, y_center, w, h
+
+
+def yolo2xyxy(
+    xc: int,
+    yc: int,
+    box_w: int,
+    box_h: int,
+    img_w: int,
+    img_h: int,
+) -> tuple[float, float, float, float]:
+    x1 = (xc - box_w / 2) * img_w
+    y1 = (yc - box_h / 2) * img_h
+    x2 = (xc + box_w / 2) * img_w
+    y2 = (yc + box_h / 2) * img_h
+    return x1, y1, x2, y2
